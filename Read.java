f26e7f9594a166d3850bc0710f0822d5ed92d981
Pablo <pablojacobo1998@gmail.com>
2018-09-24 23:41:16 -0600

    Fix Reader

---
+++ Read.java
@@ -0,0 +1,22 @@
+package org.hackobo.ingreso;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Ingreso{
+
+    /*public static ingresarData(){
+        String data = null;
+        BufferedReader _read = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            data = _read.readLine();
+        }(IOException ioe) {
+            System.out.println("Se equivoco al escribir" + ioe);
+             System.exit(1);
+        }
+        return  _data;
+
+    }*/
+ 
+}
