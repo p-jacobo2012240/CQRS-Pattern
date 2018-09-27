8dc6ddd634b202d6c8cbe72e54a2dd2d5a3e212a
Pablo <pablojacobo1998@gmail.com>
2018-09-26 22:15:29 -0600

    Model Movie

--- Menu.java
+++ Menu.java
@@ -1,24 +1,30 @@
 package org.hackobo.menu;
 
+import org.hackobo.ingreso.Read;
+
 public class Menu{
 
     public static void mostrarMenu(){
-         
+        
+        Read _read = new Read();
+
+        String escribe = null;
         int salir = 0;
         do{
-            System.out.println("|================|");
-            System.out.println("| Boook Store    |");
-            System.out.println("|================|");
-            System.out.println("| 1.Movies       |");
-            System.out.println("| 2.Series       |");
-            System.out.println("| 3.Libros       |");
-            System.out.println("| 4.Revistas     |");
-            System.out.println("| 5.Salir        |");
-            System.out.println("|================|");
+            System.out.println("|=====================|");
+            System.out.println("|      Boook Store    |");
+            System.out.println("|=====================|");
+            System.out.println("|      1.Movies       |");
+            System.out.println("|      2.Series       |");
+            System.out.println("|      3.Libros       |");
+            System.out.println("|      4.Revistas     |");
+            System.out.println("|      5.Salir        |");
+            System.out.println("|=====================|");
             int opcion = 1;
                 switch(opcion){
                     case 1:
-                        System.out.println("ha seleccionado la opcion 1");
+                        System.out.println("::>");
+                        escribe = _read.ingresarData(); 
                     break;
                     default:
                         System.out.println("Selecciona una opcion");
---
+++ org/Movies.java
@@ -0,0 +1,39 @@
+public class Movies{
+
+    int id_movie = 0;
+    String nombre = null;
+    String calificacion = null;
+    
+    //Constructor
+    public Movies(int id_movie, String nombre, String calificacion ){
+        this.id_movie = id_movie;
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+
+    //GETTERS
+    public int getIdMovie(){
+		return id_movie;
+	}
+	public String getNombre(){
+		return nombre;
+    }
+    
+    public String getCalificacion(){
+        return calificacion;
+    }
+
+    //SETTERS
+    public void setIdMovie(int id_movie){
+		this.id_movie = id_movie;
+    }
+    
+    public void setNombre(String nombre ){
+        this.nombre = nombre;
+    }
+
+    public void setCalificacion(String calificacion){
+        this.calificacion = calificacion;
+    }
+
+}
