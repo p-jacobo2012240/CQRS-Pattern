67b287bf8c3382826e90eb623de7b0414ed787f1
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-10-02 22:47:25 -0600

    simple connection

---
+++ src/org/hackobo/db/Connection.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hackobo.db;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Hackobo
+ */
+public class Connection {
+    Connection cnn = null;
+    public static Connection Connnected() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = (Connection) DriverManager.
+                getConnection("jdbc:mysql://localhost/keeptoo_systems", "root", "");
+            return con;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }     
+    }
+    
+}
